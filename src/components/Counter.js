cfa2e50744b9d19954365fac5b1a7e0c73074082
alex12319 <alexbrata10@gmail.com>
2022-08-08 15:41:24 +0300

    style

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,6 @@
 import React from "react";
 import useStateManager from "../store/counterStore";
+import "../style.css";
 
 function Counter() {
   const {
@@ -12,9 +13,13 @@ function Counter() {
 
   return (
     <>
-      <p>counter:{state}</p>
-      <button onClick={() => dispatch("increment")}>increment</button>
-      <button onClick={() => dispatch("decrement")}>decrement</button>
+      <div className="container">
+        <h2>Counter: {state}</h2>
+        <div className="btns">
+          <button onClick={() => dispatch("increment")}>increment</button>
+          <button onClick={() => dispatch("decrement")}>decrement</button>
+        </div>
+      </div>
     </>
   );
 }
